81f1fbf51b63d6c253c0ee89c03365360e2e5d28
Hans H.L <asporov96@gmail.com>
2018-12-18 08:33:47 +0100

    Inicio con error

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
+// Imports
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux';
+import Game from "./Game";
 
 class App extends Component {
   render() {
+    console.log(this.props.questions);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Game/>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state){
+  return{
+    state
+  };
+}
+
+export default connect(mapStateToProps)(App);
